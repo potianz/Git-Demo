5978f162e1ca4c5811f476795947fa1bb1d348cb
potianz <1648919702@qq.com>
2021-07-18 17:00:33 +0800

    尝试第四次更改

--- demo-module-1/src/com/duyi/MainTest.java
+++ demo-module-1/src/com/duyi/MainTest.java
@@ -1,7 +1,12 @@
 package com.duyi;
 
+import java.util.Scanner;
+
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("potianz123请输入信息：");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
     }
 }
