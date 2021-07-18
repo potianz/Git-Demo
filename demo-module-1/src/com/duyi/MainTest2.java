9e21c1314e556d0827dfc2bd9eb78fc7ab7037e5
potianz <1648919702@qq.com>
2021-07-18 17:59:01 +0800

    会提交了吗

--- demo-module-1/src/com/duyi/MainTest2.java
+++ demo-module-1/src/com/duyi/MainTest2.java
@@ -4,5 +4,6 @@ public class MainTest2 {
     public static void main(String[] args) {
         System.out.println("真的奇怪，为什莫上传不成功！");
         System.out.println("为什莫!!!111");
+        System.out.println("芜湖  我会提交了");
     }
 }
