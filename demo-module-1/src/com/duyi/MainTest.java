c8cbaa1dffbbf8bf0e22b4204ba93a11d5271a1f
potianz <1648919702@qq.com>
2021-07-18 17:31:26 +0800

    one  第一次更改

--- demo-module-1/src/com/duyi/MainTest.java
+++ demo-module-1/src/com/duyi/MainTest.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println("potianz 17300167570请输入信息：");
+        System.out.println("potianz 1730016757请输入信息：");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        System.out.println("密钥 zyh");
         System.out.println(a);
 
     }
