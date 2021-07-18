2c5f4a1f62523c616b1d7485547b349513dc5a2d
potianz <1648919702@qq.com>
2021-07-18 17:14:43 +0800

    1

--- demo-module-1/src/com/duyi/MainTest.java
+++ demo-module-1/src/com/duyi/MainTest.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println("potianz请输入信息：");
+        System.out.println("potianz zzt请输入信息：");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         System.out.println(a);
