276393c4e983be4da25e88f9938cd1ee662ce0be
potianz <1648919702@qq.com>
2021-07-20 15:07:11 +0800

    test 修改  hello test

--- demo-module-1/src/com/duyi/MainTest1.java
+++ demo-module-1/src/com/duyi/MainTest1.java
@@ -2,6 +2,8 @@ package com.duyi;
 
 public class MainTest1 {
     public static void main(String[] args) {
+
         System.out.println("测试一");
+        System.out.println("hello  test");
     }
 }
