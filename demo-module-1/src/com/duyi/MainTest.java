c40d419e456d21269289bbf1c88a481093c122e0
zhangyu <1648919702@qq.com>
2022-03-03 10:32:45 +0800

    测试连接和push

--- demo-module-1/src/com/duyi/MainTest.java
+++ demo-module-1/src/com/duyi/MainTest.java
@@ -4,12 +4,9 @@ import java.util.Scanner;
 
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println("potianz 1730016757请输入信息：");
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        System.out.println("密钥 zyh11zs");
-        System.out.println(a);
-        System.out.println();
+        System.out.println("今天是2022年3月3日");
+        System.out.println("设置好了git");
+        System.out.println("测试push");
 
     }
 }
