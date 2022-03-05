899113a94d8e5ec4795db82d8e776db1b03bf37c
potianz <1648919702@qq.com>
2022-03-05 10:17:55 +0800

    2021.3.4

--- demo-module-1/src/com/duyi/MainTest1.java
+++ demo-module-1/src/com/duyi/MainTest1.java
@@ -2,8 +2,6 @@ package com.duyi;
 
 public class MainTest1 {
     public static void main(String[] args) {
-        System.out.println("测试一");
-        System.out.println("hello  master");
-        System.out.println("hello  test");
+        System.out.println("one");
     }
 }
